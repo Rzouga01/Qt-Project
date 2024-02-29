1b63daa4a1e7a1f092d1d81df06df00109163d9f
Abd Razek Nakhli <abdrazek.nakhli@esprit.tn>
2024-02-29 20:03:45 +0100

    added access methods

--- Ripple/employee.h
+++ Ripple/employee.h
@@ -15,17 +15,44 @@ class Employee : public QDialog {
     Q_OBJECT
 private:
     Ui::Employee *ui;
-   QString EMAIL;
-   QString PASSWORD;
-   QString ROLE;
-   QString FIRST_NAME;
-   QString LAST_NAME;
-   QString PHONE_NUMBER;
-   QString ADDRESS;
-   QDate DOB;
+    QString EMAIL;
+    QString PASSWORD;
+    QString ROLE;
+    QString FIRST_NAME;
+    QString LAST_NAME;
+    QString PHONE_NUMBER;
+    QString ADDRESS;
+    QDate DOB;
+
 public:
     explicit Employee(QWidget *parent = nullptr);
     ~Employee();
+
+    // Getters
+    QString getEmail() const;
+    QString getPassword() const;
+    QString getRole() const;
+    QString getFirstName() const;
+    QString getLastName() const;
+    QString getPhoneNumber() const;
+    QString getAddress() const;
+    QDate getDOB() const;
+
+    // Setters
+    void setEmail(const QString &email);
+    void setPassword(const QString &password);
+    void setRole(const QString &role);
+    void setFirstName(const QString &firstName);
+    void setLastName(const QString &lastName);
+    void setPhoneNumber(const QString &phoneNumber);
+    void setAddress(const QString &address);
+    void setDOB(const QDate &dob);
+
+    // CRUD
+    bool createEmployee();
+    void readEmployee();
+    bool updateEmployee();
+    bool deleteEmployee();
 };
 
 #endif // EMPLOYEE_H
