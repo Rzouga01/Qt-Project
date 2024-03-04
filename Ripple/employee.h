e260df126e6c853de5f8216c74e15a3265e026db
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2024-03-04 15:04:11 +0100

    Fix

--- Ripple/employee.h
+++ Ripple/employee.h
@@ -15,44 +15,45 @@ class Employee : public QDialog {
     Q_OBJECT
 private:
     Ui::Employee *ui;
-    QString EMAIL;
-    QString PASSWORD;
-    QString ROLE;
-    QString FIRST_NAME;
-    QString LAST_NAME;
-    QString PHONE_NUMBER;
-    QString ADDRESS;
-    QDate DOB;
+    QString email;
+    QString password;
+     int role;
+    QString first_name;
+    QString last_name;
+    QString phone_number;
+    QString address;
+    QDate dob;
 
 public:
     explicit Employee(QWidget *parent = nullptr);
+    Employee(QString email, QString password, int role, QString first_name, QString last_name, QString phone_number, QString address, QDate dob);
     ~Employee();
 
     // Getters
     QString getEmail() const;
     QString getPassword() const;
-    QString getRole() const;
+    int getRole() const;
     QString getFirstName() const;
     QString getLastName() const;
     QString getPhoneNumber() const;
     QString getAddress() const;
-    QDate getDOB() const;
+    QDate getDob() const;
 
     // Setters
     void setEmail(const QString &email);
     void setPassword(const QString &password);
-    void setRole(const QString &role);
+    void setRole(const int &role);
     void setFirstName(const QString &firstName);
     void setLastName(const QString &lastName);
     void setPhoneNumber(const QString &phoneNumber);
     void setAddress(const QString &address);
-    void setDOB(const QDate &dob);
+    void setDob(const QDate &dob);
 
     // CRUD
     bool createEmployee();
     void readEmployee();
-    bool updateEmployee();
-    bool deleteEmployee();
+    bool updateEmployee(int id);
+    bool deleteEmployee(int id);
 };
 
 #endif // EMPLOYEE_H
