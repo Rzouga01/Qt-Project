d562e2db9eccf24c0c418c42bdb2adf2226883ae
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2024-03-07 23:47:10 +0100

    test

--- build-Ripple-Desktop_Qt_5_9_9_MinGW_32bit-Debug/debug/moc_client.cpp
+++ build-Ripple-Desktop_Qt_5_9_9_MinGW_32bit-Debug/debug/moc_client.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Client_t {
-    QByteArrayData data[1];
-    char stringdata0[7];
+    QByteArrayData data[5];
+    char stringdata0[61];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -31,10 +31,15 @@ struct qt_meta_stringdata_Client_t {
     )
 static const qt_meta_stringdata_Client_t qt_meta_stringdata_Client = {
     {
-QT_MOC_LITERAL(0, 0, 6) // "Client"
+QT_MOC_LITERAL(0, 0, 6), // "Client"
+QT_MOC_LITERAL(1, 7, 21), // "deleteClientRequested"
+QT_MOC_LITERAL(2, 29, 0), // ""
+QT_MOC_LITERAL(3, 30, 8), // "clientId"
+QT_MOC_LITERAL(4, 39, 21) // "updateClientRequested"
 
     },
-    "Client"
+    "Client\0deleteClientRequested\0\0clientId\0"
+    "updateClientRequested"
 };
 #undef QT_MOC_LITERAL
 
@@ -44,22 +49,51 @@ static const uint qt_meta_data_Client[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   24,    2, 0x06 /* Public */,
+       4,    1,   27,    2, 0x06 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::Int,    3,
 
        0        // eod
 };
 
 void Client::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Client *_t = static_cast<Client *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->deleteClientRequested((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 1: _t->updateClientRequested((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            typedef void (Client::*_t)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Client::deleteClientRequested)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            typedef void (Client::*_t)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Client::updateClientRequested)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
 }
 
 const QMetaObject Client::staticMetaObject = {
@@ -84,7 +118,32 @@ void *Client::qt_metacast(const char *_clname)
 int Client::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
     return _id;
+}
+
+// SIGNAL 0
+void Client::deleteClientRequested(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void Client::updateClientRequested(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
