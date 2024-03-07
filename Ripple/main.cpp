59965cd8ad53bd7d3a7f6b5a9f54e1a6b4a17b5e
Malek Bsaissa <atfastr@gmail.com>
2024-03-07 01:49:59 +0100

    main changes

--- Ripple/main.cpp
+++ Ripple/main.cpp
@@ -18,13 +18,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     MainWindow w;
-    Client c("email", "first_name", "last_name", "12345678", "address", QDate::currentDate());
     //Employee e("email", "password", 0, "first_name", "last_name", "12345678", "address", QDate::currentDate());
     contract ct(1,1,100,QDate::currentDate(),QDate::currentDate(),0,"test");
     //accident ac; 
     Connection con;
     Dashboard dash;
-    Client c3("test", "test", "test", "test", "test", QDate::currentDate());
 
    
 
